ac4e41452a150356f601ab4448645fdfc5c9bb03
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2022-10-27 07:29:08 -0400

    fixed a bug where the OrderPage would crash after reload

--- src/UcabGo/components/PedidoForm.jsx
+++ src/UcabGo/components/PedidoForm.jsx
@@ -5,16 +5,9 @@ import { useAuthStore, useUcabGoStore } from "../../hooks";
 import { getProductById } from "../helpers";
 import { getTime } from "../helpers";
 
-export const PedidoForm = ({ id }) => {
+export const PedidoForm = ({ product }) => {
   const navigate = useNavigate();
   const time = getTime();
-  const { startLoadingProducts } = useUcabGoStore();
-
-  useEffect(() => {
-    startLoadingProducts();
-  }, []);
-
-  const product = getProductById(id);
 
   const { user } = useAuthStore();
   // const itemImageUrl = product.img;
@@ -46,145 +39,135 @@ export const PedidoForm = ({ id }) => {
   };
 
   return (
-    <>
-      {!product ? (
-        <h3>Cargando...</h3>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <div className="form-group row">
-            <label htmlFor="ubicacion" className="col-sm-2 col-form-label">
-              Ubicacion
-            </label>
-            <div className="col-sm-10">
-              <select
-                className="form-control"
-                name="ubicacion"
-                value={formValues.ubicacion}
-                onChange={onInputChanged}
-                required
-              >
-                <option>Modulos</option>
-                <option>Laboratorios</option>
-                <option>Feria</option>
-                <option>Solarium</option>
-                <option>Cincuentenario</option>
-                <option>Cafetin</option>
-                <option>Canchas</option>
-                <option>Otro</option>
-              </select>
-            </div>
-          </div>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group row">
+        <label htmlFor="ubicacion" className="col-sm-2 col-form-label">
+          Ubicacion
+        </label>
+        <div className="col-sm-10">
+          <select
+            className="form-control"
+            name="ubicacion"
+            value={formValues.ubicacion}
+            onChange={onInputChanged}
+            required
+          >
+            <option>Modulos</option>
+            <option>Laboratorios</option>
+            <option>Feria</option>
+            <option>Solarium</option>
+            <option>Cincuentenario</option>
+            <option>Cafetin</option>
+            <option>Canchas</option>
+            <option>Otro</option>
+          </select>
+        </div>
+      </div>
 
-          <div className="form-group row">
-            <label htmlFor="ubicacion" className="col-sm-2 col-form-label">
-              Descripcion
-            </label>
-            <div className="col-sm-10">
+      <div className="form-group row">
+        <label htmlFor="ubicacion" className="col-sm-2 col-form-label">
+          Descripcion
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Descripcion detallada de su ubicacion"
+            name="desc"
+            value={formValues.desc}
+            onChange={onInputChanged}
+            required
+          />
+        </div>
+      </div>
+
+      <fieldset className="form-group">
+        <div className="row">
+          <legend className="col-form-label col-sm-2 pt-0">
+            Metodo de Pago
+          </legend>
+          <div className="col-sm-10">
+            <div className="form-check">
               <input
-                type="text"
-                className="form-control"
-                placeholder="Descripcion detallada de su ubicacion"
-                name="desc"
-                value={formValues.desc}
+                className="form-check-input"
+                type="radio"
+                name="pago"
+                value="Efectivo"
                 onChange={onInputChanged}
                 required
               />
+              <label className="form-check-label" htmlFor="gridRadios1">
+                Efectivo
+              </label>
             </div>
-          </div>
-
-          <fieldset className="form-group">
-            <div className="row">
-              <legend className="col-form-label col-sm-2 pt-0">
-                Metodo de Pago
-              </legend>
-              <div className="col-sm-10">
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="pago"
-                    value="Efectivo"
-                    onChange={onInputChanged}
-                    required
-                  />
-                  <label className="form-check-label" htmlFor="gridRadios1">
-                    Efectivo
-                  </label>
-                </div>
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="pago"
-                    value="Pago Movil"
-                    onChange={onInputChanged}
-                  />
-                  <label className="form-check-label" htmlFor="gridRadios2">
-                    Pago Movil
-                  </label>
-                </div>
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="pago"
-                    value="tarjeta"
-                    onChange={onInputChanged}
-                  />
-                  <label className="form-check-label" htmlFor="tarjeta">
-                    Tarjeta
-                  </label>
-                </div>
-              </div>
-            </div>
-          </fieldset>
-
-          <div className="form-group row">
-            <label htmlFor="apariencia" className="col-sm-2 col-form-label">
-              Apariencia
-            </label>
-            <div className="col-sm-10">
+            <div className="form-check">
               <input
-                type="text"
-                className="form-control"
-                id="apariencia"
-                placeholder="Ej. vestimenta, pantalon, etc."
-                name="apariencia"
-                value={formValues.apariencia}
+                className="form-check-input"
+                type="radio"
+                name="pago"
+                value="Pago Movil"
                 onChange={onInputChanged}
-                required
               />
+              <label className="form-check-label" htmlFor="gridRadios2">
+                Pago Movil
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="pago"
+                value="tarjeta"
+                onChange={onInputChanged}
+              />
+              <label className="form-check-label" htmlFor="tarjeta">
+                Tarjeta
+              </label>
             </div>
           </div>
-          <h3 className="h3 mt-5">Confirme su Pedido!</h3>
-          <hr />
-          <div className="card mb-3" style={{ width: "18rem" }}>
-            <img
-              className="card-img-top"
-              src={product.name}
-              alt={product.name}
-            />
-            <div className="card-body">
-              <h5 className="card-title">{product.name}</h5>
-              <p className="card-text">{product.desc}</p>
-              <p className="card-text">
-                Total:
-                <b> {product.price}$</b>
-              </p>
-            </div>
-          </div>
-          <div className="form-group row">
-            <div className="col-sm-10">
-              <button type="submit" className="btn btn-success">
-                Confirmar Pedido!
-              </button>
-              <Link className="btn btn-danger ml-3" to={storeLink}>
-                Cancelar
-              </Link>
-            </div>
-          </div>
-        </form>
-      )}
-    </>
+        </div>
+      </fieldset>
+
+      <div className="form-group row">
+        <label htmlFor="apariencia" className="col-sm-2 col-form-label">
+          Apariencia
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="apariencia"
+            placeholder="Ej. vestimenta, pantalon, etc."
+            name="apariencia"
+            value={formValues.apariencia}
+            onChange={onInputChanged}
+            required
+          />
+        </div>
+      </div>
+      <h3 className="h3 mt-5">Confirme su Pedido!</h3>
+      <hr />
+      <div className="card mb-3" style={{ width: "18rem" }}>
+        <img className="card-img-top" src={product.name} alt={product.name} />
+        <div className="card-body">
+          <h5 className="card-title">{product.name}</h5>
+          <p className="card-text">{product.desc}</p>
+          <p className="card-text">
+            Total:
+            <b> {product.price}$</b>
+          </p>
+        </div>
+      </div>
+      <div className="form-group row">
+        <div className="col-sm-10">
+          <button type="submit" className="btn btn-success">
+            Confirmar Pedido!
+          </button>
+          <Link className="btn btn-danger ml-3" to={storeLink}>
+            Cancelar
+          </Link>
+        </div>
+      </div>
+    </form>
   );
 };
--- src/UcabGo/pages/OrderPage.jsx
+++ src/UcabGo/pages/OrderPage.jsx
@@ -1,14 +1,24 @@
+import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useUcabGoStore } from "../../hooks";
 import { PedidoForm } from "../components";
+import { getProductById } from "../helpers";
 
 export const OrderPage = () => {
   const { id } = useParams();
+  const { startLoadingProducts } = useUcabGoStore();
+
+  useEffect(() => {
+    startLoadingProducts();
+  }, []);
+
+  const product = getProductById(id);
+
   return (
     <>
       <h1>Casi Listo!</h1>
       <hr />
-
-      <PedidoForm id={id} />
+      {!product ? <h1>Cargando...</h1> : <PedidoForm product={product} />}
     </>
   );
 };
